950e9f333e427992138df3f7a6fdd79a6268f88e
Diego <diego171200@gmail.com>
2022-01-11 10:39:11 +0100

    Estructura del Proyecto
    Creación de la extensión de colores y parte de la estructura del proyecto.

---
+++ Sport4All/Sport4All/Util/Extension/ColorExtension.swift
@@ -0,0 +1,23 @@
+//
+//  ColorExtension.swift
+//  Sport4All
+//
+//  Created by Diego Muñoz Herranz on 11/1/22.
+//
+
+import UIKit
+
+extension UIColor {
+	
+	// Colors
+	static let backgroundColor = UIColor(named: "BackgroundColor")
+	static let blueBerry = UIColor(named: "BlueBerry")
+	static let calendarColor = UIColor(named: "CalendarColor")
+	static let corporativeColor = UIColor(named: "CorporativeColor")
+	static let goldColor = UIColor(named: "GoldColor")
+	static let hardColor = UIColor(named: "HardColor")
+	static let periwinkle = UIColor(named: "Periwinkle")
+	static let redColor = UIColor(named: "RedColor")
+	static let searchColor = UIColor(named: "SearchColor")
+	static let softBlue = UIColor(named: "SoftBlue")
+}
