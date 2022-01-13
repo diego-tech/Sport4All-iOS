3e4333ddd844379ed05516e7c12bbdd2c137979f
Diego <diego171200@gmail.com>
2022-01-13 11:49:40 +0100

    Fuentes Custom

---
+++ Sport4All/Sport4All/Util/TextStyles.swift
@@ -0,0 +1,22 @@
+//
+//  TextStyles.swift
+//  Sport4All
+//
+//  Created by Diego Muñoz Herranz on 13/1/22.
+//
+
+import UIKit
+
+struct TextStyles {
+	
+	enum FontType: String {
+		case SFProRegular = "SFPro-Regular"
+		case SFProMedium = "SFPro-Medium"
+		case SFProSemibold = "SFPro-Semibold"
+		case SFProBold = "SFPro-Bold"
+		case SFProDisplayRegular = "SFProDisplay-Regular"
+		case SFProDisplayMedium = "SFProDisplay-Medium"
+		case SFProDisplaySemibold = "SFProDisplay-Semibold"
+		case SFProDisplayBold = "SFProDisplay-Bold"
+	}
+}
--- Sport4All/Sport4All/ViewController.swift
+++ Sport4All/Sport4All/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var exampleLabel: UILabel!
+		
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+	
+		exampleLabel.font = UIFont(name: TextStyles.FontType.SFProMedium.rawValue, size: 12)
 	}
 }
 
