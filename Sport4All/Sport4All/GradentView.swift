6f3548b3b5d4c8389b77c8f73fbfb8b70430db3a
Cristóbal Lletget Luque <cristo.luke99@gmail.com>
2022-01-18 19:21:44 +0100

    Launch Screen

--- Sport4All/Sport4All/GradentView.swift
+++ Sport4All/Sport4All/GradentView.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import UIKit
+
+class GradientView: UIView{
+
+    private var gradientLayer = CAGradientLayer()
+    private var vertical: Bool = false
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        gradientLayer.frame = self.bounds
+        
+        if gradientLayer.sublayers == nil{
+            
+            gradientLayer.startPoint = CGPoint(x: 0, y:0)
+            gradientLayer.endPoint = vertical ? CGPoint(x:0, y:1): CGPoint(x:1, y:0)
+            gradientLayer.colors = [UIColor.purple.cgColor, UIColor.cyan.cgColor, UIColor.yellow.cgColor]
+            
+            gradientLayer.locations = [0.0,0.5,1.0]
+            self.layer.insertSublayer(gradientLayer, at: 0)
+        
+        
+        
+        }
+            
+        
+    }
+    
+}
