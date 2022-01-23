7ec0181f550073c353f70f160716820d3b436037
Diego <diego171200@gmail.com>
2022-01-23 17:53:08 +0100

    Modify Login to main and tested

--- Sport4All/Sport4All/Controllers/ViewController.swift
+++ Sport4All/Sport4All/Controllers/ViewController.swift
@@ -9,8 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 	
+	// Variables
+	
+	// Outlets
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	@IBOutlet weak var rememberPasswordButton: UIButton!
+	@IBOutlet weak var accessButton: UIButton!
+	@IBOutlet weak var goToRegisterButton: UIButton!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+	}
+	
+	// MARK: Functions
+	private func checkTextFields() {
+		if emailTextField.text == "" || passwordTextField.text == ""  {
+			textFieldErrorStyles(textField: emailTextField, placeHolderText: "Introduzca el Correo Electrónico")
+			textFieldErrorStyles(textField: passwordTextField, placeHolderText: "Introduzca la Contraseña")
+		} else {
+			debugPrint("Continua!!")
+		}
+	}
+	
+	// MARK: Styles
+	private func textFieldErrorStyles(textField: UITextField, placeHolderText: String){
+		textField.layer.borderColor = UIColor.redColor?.cgColor
+		textField.layer.borderWidth = 1
+		textField.placeholder = placeHolderText
+		textField.layer.cornerCurve = .circular
+		textField.layer.cornerRadius = 5
+	}
+	
+	// MARK: Action Functions
+	@IBAction func rememberPasswordButtonAction(_ sender: UIButton) {
+	}
+	
+	@IBAction func accessButtonAction(_ sender: UIButton) {
+		checkTextFields()
+	}
+	
+	@IBAction func goToRegisterButtonAction(_ sender: Any) {
 	}
 }
