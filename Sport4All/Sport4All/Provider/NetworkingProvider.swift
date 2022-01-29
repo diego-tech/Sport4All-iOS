56e96b4a970b459627507f1a76f8c9fb981d6394
Diego <diego171200@gmail.com>
2022-01-29 12:17:30 +0100

    Add Decoder, UserDefaults File and Networkin Provider File

---
+++ Sport4All/Sport4All/Provider/NetworkingProvider.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkingProvider.swift
+//  Sport4All
+//
+//  Created by Diego Muñoz Herranz on 29/1/22.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkingProvider {
+	static let shared = NetworkingProvider()
+	
+}
