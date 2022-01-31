dfed06b47abbbd3fd77e3952fb4a6554b0194e8e
Diego <diego171200@gmail.com>
2022-01-31 18:13:35 +0100

    Implementación Rutas API
    - Recuperar Contraseña
    - Modificar Datos
    - Modificar Contraseña
    - Registrar Favoritos
    - Listado de Clubes

--- Sport4All/Sport4All/Model/DbResponse.swift
+++ Sport4All/Sport4All/Model/DbResponse.swift
@@ -20,6 +20,18 @@ struct Response: Decodable {
 }
 
 
+struct ListResponse: Decodable {
+	let status: Int?
+	let data: [Data]?
+	let msg: String?
+	
+	enum CodingKeys: String, CodingKey {
+		case status
+		case data
+		case msg
+	}
+}
+
 //struct Response: Decodable {
 //	let status: Int?
 //	let data: Data?
