bfd450a9e0b4421641f34af78f10202a52f32a7d
Diego <diego171200@gmail.com>
2022-02-04 14:14:23 +0100

    Fix Best Rated Cell

---
+++ Sport4All/Sport4All/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Sport4All
+//
+//  Created by Diego Muñoz Herranz on 4/2/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
