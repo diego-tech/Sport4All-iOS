feb5c03e64e2cfd8b5d91eb4db3de6ad2f836bc1
JaviGondel <javiergonzalezdelmas@gmail.com>
2022-02-08 12:47:28 +0100

    Create Club View Cell

---
+++ Sport4All/Sport4All/Views/CellsView/ClubCell/ClubTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ClubTableViewCell.swift
+//  Sport4All
+//
+//  Created by Javier González Delmas on 8/2/22.
+//
+
+import UIKit
+
+class ClubTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
