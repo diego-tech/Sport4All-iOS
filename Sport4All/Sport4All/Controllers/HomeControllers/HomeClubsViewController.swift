9c99041b41f684c1b19890c92dd072657f382e79
Diego <diego171200@gmail.com>
2022-02-16 13:52:16 +0100

    Add Custom Home Club Table View Cell

--- Sport4All/Sport4All/Controllers/HomeControllers/HomeClubsViewController.swift
+++ Sport4All/Sport4All/Controllers/HomeControllers/HomeClubsViewController.swift
@@ -14,6 +14,7 @@ class HomeClubsViewController: UIViewController {
 	// Outlets
 	@IBOutlet weak var searchBar: UITextField!
 	@IBOutlet weak var bestRatedCollectionView: UICollectionView!
+	@IBOutlet weak var homeClubsTableView: UITableView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -25,7 +26,19 @@ class HomeClubsViewController: UIViewController {
 		bestRatedCollectionView.isScrollEnabled = true
 		bestRatedCollectionView.register(UINib.init(nibName: "BestRatedCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "cell")
 		bestRatedCollectionView.backgroundColor = UIColor.clear.withAlphaComponent(0)
+		bestRatedCollectionView.showsVerticalScrollIndicator = false
+		bestRatedCollectionView.showsHorizontalScrollIndicator = false
 
+		// Inicialización Table View
+		homeClubsTableView.dataSource = self
+		homeClubsTableView.delegate = self
+		homeClubsTableView.isScrollEnabled = true
+		homeClubsTableView.register(UINib.init(nibName: "ClubTableViewCell", bundle: nil), forCellReuseIdentifier: "HomeClubTableViewCell")
+		homeClubsTableView.backgroundColor = UIColor.clear.withAlphaComponent(0)
+		homeClubsTableView.separatorStyle = .none
+		homeClubsTableView.showsHorizontalScrollIndicator = false
+		homeClubsTableView.showsVerticalScrollIndicator = false
+		
 		// Custom Search Bar
 		searchBar.customSearch()
 	}
@@ -43,13 +56,24 @@ extension HomeClubsViewController: UICollectionViewDelegate, UICollectionViewDel
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? BestRatedCollectionViewCell
-		return cell!
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! BestRatedCollectionViewCell
+		return cell
 	}
 	
 	/* Márgenes entre las celdas */
 	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
 		let inset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
 		return inset
+	}
+}
+
+extension HomeClubsViewController: UITableViewDataSource, UITableViewDelegate {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = homeClubsTableView.dequeueReusableCell(withIdentifier: "HomeClubTableViewCell") as! ClubTableViewCell
+		return cell
 	}
 }
