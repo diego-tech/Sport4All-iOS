38601be6582304354710df9121375297e1f69338
Diego Muñoz Herranz <diego171200@gmail.com>
2022-02-23 19:14:13 +0100

    Add Manually Pending Event List
    Comprobación si tenemos todos hasta el día de hoy.

--- Sport4All/Sport4All/Controllers/HomeControllers/HomeViewController.swift
+++ Sport4All/Sport4All/Controllers/HomeControllers/HomeViewController.swift
@@ -56,13 +56,14 @@ class HomeViewController: UIViewController {
 	}
 	
 	@IBAction func testButtonAction(_ sender: UIButton) {
-//		let vc = UIStoryboard(name: "InfoClub", bundle: nil).instantiateViewController(withIdentifier: "InfoClub") as! InfoClubViewController
+		let vc = UIStoryboard(name: "InfoClub", bundle: nil).instantiateViewController(withIdentifier: "InfoClub") as! InfoClubViewController
 //		let vc = UIStoryboard(name: "DetailEvent", bundle: nil).instantiateViewController(withIdentifier: "DetailEvent") as! DetailEventViewController
 //		let vc = UIStoryboard(name: "ModifyPassword", bundle: nil).instantiateViewController(withIdentifier: "ModifyPassword") as! ModifyPasswordViewController
 //		let vc = UIStoryboard(name: "YourClub", bundle: nil).instantiateViewController(withIdentifier: "YourClub") as! YourClubViewController
 //		let vc = UIStoryboard(name: "PendingEventsDetail", bundle: nil).instantiateViewController(withIdentifier: "PendingEvents") as! PendingEventsViewController
 		
-		let vc = UIStoryboard(name: "PayResume", bundle: nil).instantiateViewController(withIdentifier: "PayResume") as! PayResumeViewController
+//		let vc = UIStoryboard(name: "PayResume", bundle: nil).instantiateViewController(withIdentifier: "PayResume") as! PayResumeViewController
+//		let vc = UIStoryboard(name: "EventPendingList", bundle: nil).instantiateViewController(withIdentifier: "EventPendingList") as! EventPendingListViewController
 
 		show(vc, sender: self)
 	}
