450d8edd8a598e56cda7a5b0bd4132f6303f184f
Diego Muñoz Herranz <diego171200@gmail.com>
2022-02-24 16:51:17 +0100

    UIImageview Club Cell Styles
    Redondeo de los bordes de la imagen

--- Sport4All/Sport4All/Util/Extension/UIImageViewExtension.swift
+++ Sport4All/Sport4All/Util/Extension/UIImageViewExtension.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 extension UIImageView {
-	// Imagen Redonda
 	
+	// Imagen Redonda
 	func makeRounds() {
 		self.layer.borderWidth = 0
 		self.layer.masksToBounds = false
 		self.layer.cornerRadius = self.frame.height / 2
 		self.clipsToBounds = true
+	}
+	
+	// Redondeo de dos bordes
+	public func roundOnlyTwoCorners(_ corners: UIRectCorner, radius: CGFloat) {
+		let maskPath = UIBezierPath(roundedRect: bounds,
+									byRoundingCorners: corners,
+									cornerRadii: CGSize(width: radius, height: radius))
+		let shape = CAShapeLayer()
+		shape.path = maskPath.cgPath
+		layer.mask = shape
 	}
 }
--- Sport4All/Sport4All/Views/CellsView/ClubCell/ClubTableViewCell.swift
+++ Sport4All/Sport4All/Views/CellsView/ClubCell/ClubTableViewCell.swift
@@ -23,29 +23,34 @@ class ClubTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
 		
-		// Inicialización Estilos Vista
-		homeClubsUIView.layer.cornerCurve = .circular
-		homeClubsUIView.layer.cornerRadius = 10
-		homeClubsUIView.backgroundColor = .softBlue
-		homeClubsUIView.shadow()
-		
+		// Inicialización Estilos
+		uiViewStyles()
+		clubImageView.roundOnlyTwoCorners([.bottomLeft, .topLeft], radius: 10)
 		self.backgroundColor = .backgroundColor
     }
 	
+	// MARK: Action Functions
+	
+	// MARK: Functions
 	func setCellWithValueOf(_ club: Club) {
 		updateUI(clubName: club.name, clubPhone: club.tlf, clubImageView: club.club_banner)
 	}
 	
 	private func updateUI(clubName: String?, clubPhone: String?, clubImageView: String?) {
+		guard let clubImageView = clubImageView else { return }
+
 		self.clubNameLabel.text = clubName
-		self.clubPhoneLabel.text = clubPhone
-		
-		let url = URL(string: clubImageView!)
-		self.clubImageView.kf.setImage(with: url)
+		self.clubPhoneLabel.text = clubPhone		
+
+		let url = URL(string: clubImageView)
+		self.clubImageView.kf.setImage(with: url, placeholder: UIImage(named: "All Clubs Image"))
+	}
+	
+	// MARK: Styles
+	private func uiViewStyles() {
+		homeClubsUIView.layer.cornerCurve = .circular
+		homeClubsUIView.layer.cornerRadius = 10
+		homeClubsUIView.backgroundColor = .softBlue
+		homeClubsUIView.shadow()
 	}
 }
-
-/**
- let url = URL(string: "https://example.com/image.png")
- imageView.kf.setImage(with: url)
- */
