0b2d7425328d70ee977883641830df0533fed3fa
Diego Muñoz Herranz <diego171200@gmail.com>
2022-02-24 10:30:54 +0100

    Event Finish Detail (Not Tested)

---
+++ Sport4All/Sport4All/Controllers/FinishEventDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FinishEventDetailViewController.swift
+//  Sport4All
+//
+//  Created by Diego Muñoz Herranz on 24/2/22.
+//
+
+import UIKit
+
+class FinishEventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
