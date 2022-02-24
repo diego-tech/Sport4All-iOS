d3ac803cc6e28b00b41fcc7da10419644c525ad3
Diego Muñoz Herranz <diego171200@gmail.com>
2022-02-24 18:57:54 +0100

    Check If Merge is Correct

--- Sport4All/Sport4All/Controllers/HomeControllers/HomeViewController.swift
+++ Sport4All/Sport4All/Controllers/HomeControllers/HomeViewController.swift
@@ -65,8 +65,7 @@ class HomeViewController: UIViewController {
 //		let vc = UIStoryboard(name: "PayResume", bundle: nil).instantiateViewController(withIdentifier: "PayResume") as! PayResumeViewController
 //		let vc = UIStoryboard(name: "EventPendingList", bundle: nil).instantiateViewController(withIdentifier: "EventPendingList") as! EventPendingListViewController
 //		let vc = UIStoryboard(name: "EditProfile", bundle: nil).instantiateViewController(withIdentifier: "EditProfile") as! EditProfileViewController
-
-		let vc = UIStoryboard(name: "RegisterInEvent", bundle: nil).instantiateViewController(withIdentifier: "RegisterInEvent") as! RegisterInEventViewController
+//		let vc = UIStoryboard(name: "RegisterInEvent", bundle: nil).instantiateViewController(withIdentifier: "RegisterInEvent") as! RegisterInEventViewController
 		let vc = UIStoryboard(name: "FinishEventDetail", bundle: nil).instantiateViewController(withIdentifier: "FinishEventDetail") as! FinishEventDetailViewController
 		
 		show(vc, sender: self)
