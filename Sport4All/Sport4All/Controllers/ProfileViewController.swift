e3494669e2ca40a656419493a9d32104951cb35c
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-01 13:27:15 +0100

    User Profile With API, Name and Image Placeholder

--- Sport4All/Sport4All/Controllers/ProfileViewController.swift
+++ Sport4All/Sport4All/Controllers/ProfileViewController.swift
@@ -6,18 +6,22 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ProfileViewController: UIViewController {
 	
 	// Variables
-	
+	private var userImage: String?
+	private var userName: String?
 	
 	// Outlets
-	@IBOutlet weak var headerUIView: UIView?
-	@IBOutlet weak var settingsBTN: UIButton?
-	@IBOutlet weak var pendingEventsBTN: UIButton?
-	@IBOutlet weak var completedEventsBTN: UIButton?
-	@IBOutlet weak var yourClubBTN: UIButton?
+	@IBOutlet weak var headerUIView: UIView!
+	@IBOutlet weak var settingsBTN: UIButton!
+	@IBOutlet weak var pendingEventsBTN: UIButton!
+	@IBOutlet weak var completedEventsBTN: UIButton!
+	@IBOutlet weak var yourClubBTN: UIButton!
+	@IBOutlet weak var userImageView: UIImageView!
+	@IBOutlet weak var userNameLabel: UILabel!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +29,29 @@ class ProfileViewController: UIViewController {
 		
 		// Inicialización Estilos
 		headerUIView?.bottomShadow()
+		
+		// Api
+		fetchUserInfo()
     }
 	
 	// MARK: Action Functions
 	
-	
 	// MARK: Functions
+	private func fetchUserInfo() {
+		NetworkingProvider.shared.userInfo { responseData, status, msg in
+			guard let userName = responseData?.name else { return }
+			guard let userImage = responseData?.image else { return }
+			guard let userSurname = responseData?.surname else { return }
+			
+			let allUserName = userName + " " + userSurname
+			self.userNameLabel.text = allUserName
+			
+			let url = URL(string: Constants.kStorageURL + userImage)
+			self.userImageView.kf.setImage(with: url, placeholder: UIImage(named: "Avatar"))
+		} failure: { error in
+			print(error)
+		}
+	}
 
 	// MARK: Styles
 }
