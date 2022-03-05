b87150b9c86b2ffb3ac941007318f0115082f1c8
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-05 10:46:23 +0100

    List Favs
    - Listado clubes favoritos se ve correctamente
    - Extensión UIStackView con función setServicesInStackView

---
+++ Sport4All/Sport4All/Util/Extension/UIStackViewExtension.swift
@@ -0,0 +1,32 @@
+//
+//  UIStackViewExtension.swift
+//  Sport4All
+//
+//  Created by Diego Muñoz Herranz on 5/3/22.
+//
+
+import UIKit
+
+extension UIStackView {
+	
+	// Get array services and set images in StackView
+	func setServicesInStackView(services: [ClubService]) {
+		self.arrangedSubviews.forEach { $0.removeFromSuperview() }
+		
+		var servicesImages = [UIImage()]
+		
+		for service in services {
+			servicesImages.append(GetServices.getServices(clubService: service))
+		}
+				
+		for servicesImage in servicesImages {
+			let serviceImageView: UIImageView = {
+				let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 21, height: 20))
+				image.tintColor = .corporativeColor
+				return image
+			}()
+			serviceImageView.image = servicesImage
+			self.addArrangedSubview(serviceImageView)
+		}
+	}
+}
