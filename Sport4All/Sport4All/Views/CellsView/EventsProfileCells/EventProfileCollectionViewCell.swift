bf365688f51c7e524d42d849eedc938721f1bac7
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-14 19:36:44 +0100

    Add Event Profile Collection View Cell

---
+++ Sport4All/Sport4All/Views/CellsView/EventsProfileCells/EventProfileCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  EventProfileCollectionViewCell.swift
+//  Sport4All
+//
+//  Created by Diego Muñoz Herranz on 14/3/22.
+//
+
+import UIKit
+
+class EventProfileCollectionViewCell: UICollectionViewCell {
+    
+}
