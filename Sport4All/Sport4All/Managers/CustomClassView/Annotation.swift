a1facd92eefa3fc0108d64041b517d2d4578f1ae
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-19 19:41:24 +0100

    Annotation Mark in external file

--- Sport4All/Sport4All/Managers/CustomClassView/Annotation.swift
+++ Sport4All/Sport4All/Managers/CustomClassView/Annotation.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import MapKit
+import Contacts
+
+// MARK: Annotation Class
+class Annotation: NSObject, MKAnnotation {
+	var coordinate: CLLocationCoordinate2D
+	var title: String?
+	var subtitle: String?
+	
+	init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+		self.coordinate = coordinate
+		self.title = title
+		self.subtitle = subtitle
+		
+		super.init()
+	}
+	
+	var mapItem: MKMapItem? {
+		guard let location = title else { return nil }
+		let addressDict = [CNPostalAddressStreetKey: location]
+		let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: addressDict)
+		let mapItem = MKMapItem(placemark: placemark)
+		mapItem.name = title
+		return mapItem
+	}
+}
