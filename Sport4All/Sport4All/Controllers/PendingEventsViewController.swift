198d815c8716bd7c5b4a476cd7afb7b29389850d
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-19 19:38:49 +0100

    Add Pending Events Detail for different data (Add more info and scroll)

--- Sport4All/Sport4All/Controllers/PendingEventsViewController.swift
+++ Sport4All/Sport4All/Controllers/PendingEventsViewController.swift
@@ -6,25 +6,33 @@
 //
 
 import UIKit
+import MapKit
+import Contacts
 
 class PendingEventsViewController: UIViewController {
 	
 	// MARK: Variables
 	var pendingEvent: PendingOrFinishEvent?
 	var pendingType: PendingType?
+	var barcode: String?
+	var location: Location?
 	
-	// MARK: Matches and Reserves Outlets
+	// MARK: Outlets
 	@IBOutlet weak var headerUIView: UIView!
 	@IBOutlet weak var barcodeImageView: UIImageView!
-	
-	// MARK: Events Outlest
+	@IBOutlet weak var mapView: MKMapView!
 	@IBOutlet weak var eventHeaderUIView: UIView!
 	@IBOutlet weak var eventImageView: LazyImageView!
-	@IBOutlet weak var eventNameLabel: UILabel!
+	@IBOutlet weak var headerTitleLabel: UILabel!
+	@IBOutlet weak var courtInfoLabel: UILabel!
+	@IBOutlet weak var courtLabel: UILabel!
 	
-    override func viewDidLoad() {
+	override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+		
+		// Map View Delegate
+		mapView.delegate = self
 		
 		// Configure Navbar
 		configureNavbar()
@@ -34,11 +42,22 @@ class PendingEventsViewController: UIViewController {
 		eventHeaderUIView.bottomShadow()
 		barcodeIVStyle()
 		
+		// Barcode Image View Action
+		let gesture = UITapGestureRecognizer(target: self, action: #selector(barcodeTapped))
+		barcodeImageView.isUserInteractionEnabled = true
+		barcodeImageView.addGestureRecognizer(gesture)
+		
 		// Configure
 		configure()
     }
 	
 	// MARK: Action Functions
+	@objc func barcodeTapped() {
+		let vc = UIStoryboard(name: "QrDetail", bundle: nil).instantiateViewController(withIdentifier: "QRDetail") as! QRDetailViewController
+		guard let barcode = barcode else { return }
+		vc.qrCode = barcode
+		present(vc, animated: true)
+	}
 	
 	// MARK: Functions
 	private func configure() {
@@ -64,9 +83,12 @@ class PendingEventsViewController: UIViewController {
 		guard let eventImage = pendingEvent.eventImg else { return debugPrint("Error Event Image") }
 		guard let imgUrl = URL(string: Constants.kStorageURL + eventImage) else { return debugPrint("Error Event URL Image") }
 		guard let eventName = pendingEvent.eventName else { return debugPrint("Error Nombre de Evento") }
+		guard let pedingLocation = pendingEvent.clubLocation else { return debugPrint("Error Location") }
 		
+		print("Location \(pedingLocation)")
 		self.eventImageView.loadImage(fromURL: imgUrl)
-		self.eventNameLabel.text = eventName 
+		self.headerTitleLabel.text = eventName
+		self.callFindLocation(locationName: pedingLocation)
 	}
 	
 	private func configurePendingMatchOrReserve() {
@@ -74,9 +96,49 @@ class PendingEventsViewController: UIViewController {
 		
 		guard let pendingEvent = pendingEvent else { return debugPrint("Error Evento") }
 		guard let pendingBarcode = pendingEvent.qr else { return debugPrint("Error Qr") }
-		
+		guard let pendingTitle = pendingEvent.clubName else { return }
+		guard let pendingCourt = pendingEvent.courtName else { return }
+		guard let pendingTypeCourt = pendingEvent.courtType else { return }
+		guard let location = pendingEvent.clubLocation else { return }
+		print("Location \(location)")
+
+		self.barcode = pendingBarcode
 		
 		self.barcodeImageView.image = AuxFunctions.generateQRCodeImage(reservationCode: pendingBarcode)
+		self.headerTitleLabel.text = pendingTitle
+		self.courtLabel.text = "- \(pendingCourt) (\(pendingTypeCourt))"
+		self.callFindLocation(locationName: location)
+	}
+	
+	private func callFindLocation(locationName: String) {
+		LocationProvider.shared.findLocation(with: locationName) { [weak self] location in
+			DispatchQueue.main.async {
+				self?.location = location
+				self?.configureMapView()
+			}
+		}
+	}
+	
+	private func configureMapView() {
+		mapView.removeAnnotations(mapView.annotations)
+		guard let coordinates = location?.coordinates else { return }
+		guard let locationName = location?.title else { return }
+		guard let pendingEvent = pendingEvent else { return debugPrint("Error Evento") }
+		
+		guard let placeName = pendingEvent.clubName else { return }
+		
+		let annotation = Annotation(coordinate: coordinates, title: placeName, subtitle: locationName)
+		mapView.isZoomEnabled = false
+		mapView.isScrollEnabled = false
+		mapView.isPitchEnabled = false
+		mapView.isRotateEnabled = false
+		mapView.addAnnotation(annotation)
+		mapView.setRegion(MKCoordinateRegion(
+			center: coordinates,
+			span: MKCoordinateSpan(
+				latitudeDelta: 0.01,
+				longitudeDelta: 0.01)
+		), animated: true)
 	}
 	
 	// MARK: Styles
@@ -104,5 +166,27 @@ class PendingEventsViewController: UIViewController {
 	
 	@objc func popView(tapGestureRecognizer: UITapGestureRecognizer) {
 		self.navigationController?.popViewController(animated: true)
+	}
+}
+
+// MARK: MKMapView Extension
+extension PendingEventsViewController: MKMapViewDelegate {
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "something")
+		let mapsButton = UIButton(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 48, height: 48)))
+		mapsButton.setBackgroundImage(UIImage(named: "Map"), for: .normal)
+		
+		annotationView.markerTintColor = .corporativeColor
+		annotationView.canShowCallout = true
+		annotationView.calloutOffset = CGPoint(x: -5, y: 5)
+		annotationView.rightCalloutAccessoryView = mapsButton
+		annotationView.backgroundColor = .softBlue
+		return annotationView
+	}
+	
+	func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+		guard let place = view.annotation as? Annotation else { return }
+		let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+		place.mapItem?.openInMaps(launchOptions: launchOptions)
 	}
 }
