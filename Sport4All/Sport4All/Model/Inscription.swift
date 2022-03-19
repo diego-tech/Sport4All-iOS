ff5dff96228908bc23eaf1810f2f9eaf3731666a
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-19 19:34:45 +0100

    Add Qr detail, refactor Annotation in External File and Remove Files

---
+++ Sport4All/Sport4All/Model/Inscription.swift
@@ -0,0 +1,8 @@
+//
+//  Inscription.swift
+//  Sport4All
+//
+//  Created by Diego Muñoz Herranz on 19/3/22.
+//
+
+import Foundation
--- Sport4All/Sport4All/Views/QrDetail/QRDetailViewController.swift
+++ Sport4All/Sport4All/Views/QrDetail/QRDetailViewController.swift
@@ -9,37 +9,27 @@ import UIKit
 
 class QRDetailViewController: UIViewController {
 	
-	// MARK: Views
-	private lazy var dimmedBackgroundView: UIView = {
-		let view = UIView()
-		view.translatesAutoresizingMaskIntoConstraints = false
-		view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
-		return view
-	}()
+	// MARK: Variables
+	var qrCode: String = ""
 	
-	private lazy var imageView: UIImageView = {
-		let imageView = UIImageView()
-		imageView.frame = CGRect(x: 0, y: 0, width: 150, height: 150)
-		imageView.image = UIImage(systemName: "house.fill")
-		imageView.backgroundColor = .red
-		return imageView
-	}()
+	// MARK: Outlets
+	@IBOutlet weak var barcodeImageView: UIImageView!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-	
-		view.addSubview(dimmedBackgroundView)
-		view.addSubview(imageView)
 		
-		// Dismiss ViewController When Tapped Background
-		let gesture = UITapGestureRecognizer(target: self, action:  #selector (backgroundTapped(_:)))
-		dimmedBackgroundView.addGestureRecognizer(gesture)
+		// Inicialización de Estilos
+		view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+		styles()
+		
+		// Load QR
+		self.barcodeImageView.image = AuxFunctions.generateQRCodeImage(reservationCode: qrCode)
     }
 	
-	// MARK: Action Functions
-	@objc func backgroundTapped(_ sender: UITapGestureRecognizer) {
-		print("Hello")
-		self.dismiss(animated: true)
+	// MARK: Styles
+	private func styles() {
+		// Image View Styles
+		barcodeImageView.layer.magnificationFilter = CALayerContentsFilter.nearest
 	}
 }
