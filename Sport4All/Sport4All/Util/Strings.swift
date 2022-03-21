3ddcd5a81ca42eb3bed878a8a2204e3326a6de01
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-21 19:54:27 +0100

    Add Strings File for All Strings

---
+++ Sport4All/Sport4All/Util/Strings.swift
@@ -0,0 +1,25 @@
+//
+//  Strings.swift
+//  Sport4All
+//
+//  Created by Diego Muñoz Herranz on 21/3/22.
+//
+
+import Foundation
+
+struct Strings {
+	
+	// MARK: Onboarding
+	static let slide1Title = "Screen 1"
+	static let slide1Description = "En el apartado clubes, podrás encontrar información sobre los clubes en los que podrás reservar espacios deportivos."
+	static let slide2Title = "Screen 2"
+	static let slide2Description = "En el apartado partidos podrás jugar con otros usuarios que necesiten compañeros en sus partidos."
+	static let slide3Title = "Screen 3"
+	static let slide3Description = "Acceda fácilmente a los establecimientos identificándose con el código Qr que se le facilitará al realizar la reserva."
+	static let slide4Title = "Screen 4"
+	static let slide4Description = "Podrá registrarse en los eventos que los clubes realizan, como torneos, partidos amistosos, webinars y muchas más actividades."
+	static let slide5Title = "Screen 5"
+	static let slide5Description = "Controle los eventos personalizados que su club le proporcione."
+	static let slide6Title = "Screen 6"
+	static let slide6Description = "Registresé en nuesta aplicación y disfrute de la diversión de hacer deporte."
+}
